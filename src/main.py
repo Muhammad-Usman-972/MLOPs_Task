9a06df4e070023cfdcad891d466f6d58c1883ae5
EhtishamUlHassan <muhammadjalalkhan7658@gmail.com>
2023-09-04 22:38:13 +0500

    Added comments in the code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 def add(x,y):
+    #For Addition
     return x+y
 def sub(x,y):
+    #For Subtraction
     return x-y
 def mul(x,y):
+    #For Multiplication
     return x*y
 def div(x,y):
+    #For Division
     try:
         return x/y
     except ZeroDivisionError:
@@ -12,16 +16,18 @@ def div(x,y):
         return None
 
 if __name__ == "__main__":
-    
+    #Input Statement comment
     print("Enter The two numbers for calculator app: ")
     x = int(input())
     y = int(input())
+    #Operation Statements
     print("Enter operation to be performed")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
     choice = int(input())
+    #Choice Selection
     if choice == 1:
         print(add(x,y))
     elif choice == 2:
@@ -31,4 +37,5 @@ if __name__ == "__main__":
     elif choice == 4:
         print(div(x,y))
     else:
+        #Invalid Choice
         print("Invalid choice")
